f3fe9d83d08bec31b9a579bb8f666bf9dece1506
Jahmell Dorias <jahmelldorias17@gmail.com>
2025-12-17 14:23:32 -0800

    Hide API KEYS

--- Ordering-System/scripts/supabase-client.js
+++ Ordering-System/scripts/supabase-client.js
@@ -1,8 +1,8 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const supabase = createClient(
-  "https://fqfvvdqboxjiyxhfhymf.supabase.co",
-  "REMOVED",
+ `${process.env.SUPABASE_URL}`,
+  `${process.env.SUPABASE_ANON_KEY}`,
    {
     auth: {
       autoRefreshToken: true,
