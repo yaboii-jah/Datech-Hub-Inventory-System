725d25a775388afc96f06f0cb4cb82c82a273696
Jahmell Dorias <jahmelldorias17@gmail.com>
2025-12-17 14:38:29 -0800

    Added the API Keys Temporarily

--- Ordering-System/scripts/supabase-client.js
+++ Ordering-System/scripts/supabase-client.js
@@ -1,8 +1,8 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const supabase = createClient(
- `${process.env.SUPABASE_URL}`,
-  `${process.env.SUPABASE_ANON_KEY}`,
+  `https://fqfvvdqboxjiyxhfhymf.supabase.co`,
+  `sb_publishable_AL-yuTdljMuQAWBIISGthA_YDJpyKOZ`,
    {
     auth: {
       autoRefreshToken: true,
